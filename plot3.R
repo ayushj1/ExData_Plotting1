6d6ff55a2b43472cdc3bbf6580131f32b7139488
ayushj1 <jain.ayush.94@gmail.com>
2015-04-11 22:27:54 +0530

    Assignment-1
    Plots for Exploratory Data Analysis week 1

---
+++ plot3.R
@@ -0,0 +1,16 @@
+mydata <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+mydata$Date <- as.Date(mydata$Date, format="%d/%m/%Y")
+newdata <- subset(mydata, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(mydata)
+datetime <- paste(as.Date(newdata$Date), newdata$Time)
+newdata$Datetime <- as.POSIXct(datetime)
+with(newdata, {
+    plot(Sub_metering_1~Datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
